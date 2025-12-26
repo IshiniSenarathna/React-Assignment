fdc27b5c5dfb64e954dcb44f744249d810b7ceca
SENARATHNA G.R.I.U.H. EC 2021 033 <senarat-ec21033@stu.kln.ac.lk>
2025-12-26 06:27:00 +0530

    Initial React pdf 1- React Tutorial for Beginners

---
+++ src/HelloWorld.tsx
@@ -0,0 +1,12 @@
+function HelloWorld() {
+  const studentId = "EC21033";
+  const studentName = "Ishini Senarathna";
+
+  return (
+    <h1>
+      Hello World! – {studentId} – {studentName}
+    </h1>
+  );
+}
+
+export default HelloWorld;
