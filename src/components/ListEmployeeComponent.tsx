4ebe75d5088f4242d1d521399b261d901b711a27
SENARATHNA G.R.I.U.H. EC 2021 033 <senarat-ec21033@stu.kln.ac.lk>
2025-12-26 08:48:36 +0530

    Part 5:Connect React App with REST API

--- src/components/ListEmployeeComponent.tsx
+++ src/components/ListEmployeeComponent.tsx
@@ -1,4 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getEmployees, type Employee } from "../services/EmployeeService";
+
+const ListEmployeeComponent: React.FC = () => {
+  const [employees, setEmployees] = useState<Employee[]>([]);
+
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      try {
+        const data = await getEmployees();
+        setEmployees(data);
+      } catch (error) {
+        console.error("Failed to fetch employees", error);
+      }
+    };
+
+    fetchEmployees();
+  }, []);
+
+  return (
+    <div className="container mt-3">
+      <h3 className="text-primary">Employee List</h3>
+      <table className="table table-bordered table-striped">
+        <thead className="table-dark">
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.id}</td>
+              <td>{employee.firstName}</td>
+              <td>{employee.lastName}</td>
+              <td>{employee.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ListEmployeeComponent;
+
+
+/* before backend connectivity*/
+/*import React from "react";
 
 const ListEmployeeComponent: React.FC = () => {
   const employees = [
@@ -53,3 +103,4 @@ const ListEmployeeComponent: React.FC = () => {
 };
 
 export default ListEmployeeComponent;
+*/
